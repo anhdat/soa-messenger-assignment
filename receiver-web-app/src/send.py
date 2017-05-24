06ff7043c434c59012acff6a82ccba547de63fef
Dat Truong <mr.anhdat@gmail.com>
2017-05-24 12:03:00 +0700

    Make it safe when message broker's slow to start up

--- receiver-web-app/src/send.py
+++ receiver-web-app/src/send.py
@@ -1,8 +1,21 @@
 import pika
+import time
 
 
 def say_hello(message):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='message-broker', port=5672))
+    start_time = time.time()
+    while True:
+        # wait for rabbitmq
+        try:
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host='message-broker', port=5672))
+            break
+        except pika.exceptions.AMQPConnectionError:
+            print('Cannot connect yet, sleeping 3 seconds.')
+            time.sleep(3)
+        if time.time() - start_time > 60:
+            print('Could not connect after 30 seconds.')
+            exit(1)
     channel = connection.channel()
     channel.queue_declare(queue='hello')
     channel.basic_publish(exchange='',
